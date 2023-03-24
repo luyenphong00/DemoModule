ff972d3bb86841ff3d8672409cb5e9bca8437769
luyenphong <phong.luyenthanh@ivnd.com.vn>
2023-03-24 15:31:15 +0700

    Initial commit

---
+++ mylibrary/src/main/java/vn/com/ivnd/mylibrary/TestLog.kt
@@ -0,0 +1,12 @@
+package vn.com.ivnd.mylibrary
+
+import android.util.Log
+
+/**
+ * Create by phong.luyenthanh
+ */
+class TestLog {
+    fun logTest(){
+        Log.d("AAAAAAAAAAAAA", "TEST LOG ")
+    }
+}
