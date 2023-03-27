f26840a0920ed39d8cd20e440c5a09f1017e7222
luyenphong <phong.luyenthanh@ivnd.com.vn>
2023-03-27 16:54:17 +0700

    tích hợp module

--- app/src/main/java/vn/com/ivnd/demomodule/MainActivity.kt
+++ app/src/main/java/vn/com/ivnd/demomodule/MainActivity.kt
@@ -1,11 +1,18 @@
 package vn.com.ivnd.demomodule
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import vn.com.ivnd.student.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 2000)
     }
 }
